agogo233/zed

commit 3f1c091b871a2950c96fb767e18fc6fbb31486c3

Add the ability to store secret dev-only env vars in .env.secret.toml (#15557)
Release Notes:

- N/A

---------

Co-authored-by: Marshall <marshall@zed.dev>

crates/collab/src/bin/dotenv.rs
@@ -1,20 +1,8 @@
-use anyhow::anyhow;
-use std::fs;
+use collab::env::get_dotenv_vars;
 
 fn main() -> anyhow::Result<()> {
-    let env: toml::map::Map<String, toml::Value> = toml::de::from_str(
-        &fs::read_to_string("./.env.toml").map_err(|_| anyhow!("no .env.toml file found"))?,
-    )?;
-
-    for (key, value) in env {
-        let value = match value {
-            toml::Value::String(value) => value,
-            toml::Value::Integer(value) => value.to_string(),
-            toml::Value::Float(value) => value.to_string(),
-            _ => panic!("unsupported TOML value in .env.toml for key {}", key),
-        };
+    for (key, value) in get_dotenv_vars()? {
         println!("export {}=\"{}\"", key, value);
     }
-
     Ok(())
 }

crates/collab/src/env.rs
@@ -1,12 +1,29 @@
-use anyhow::anyhow;
+use anyhow::{anyhow, Result};
 use std::fs;
 
-pub fn load_dotenv() -> anyhow::Result<()> {
-    let env: toml::map::Map<String, toml::Value> = toml::de::from_str(
-        &fs::read_to_string("./crates/collab/.env.toml")
-            .map_err(|_| anyhow!("no .env.toml file found"))?,
-    )?;
+pub fn get_dotenv_vars() -> Result<Vec<(String, String)>> {
+    let mut vars = Vec::new();
+    let env_content = fs::read_to_string("./crates/collab/.env.toml")
+        .map_err(|_| anyhow!("no .env.toml file found"))?;
 
+    add_vars(env_content, &mut vars)?;
+
+    if let Ok(secret_content) = fs::read_to_string("./crates/collab/.env.secret.toml") {
+        add_vars(secret_content, &mut vars)?;
+    }
+
+    Ok(vars)
+}
+
+pub fn load_dotenv() -> Result<()> {
+    for (key, value) in get_dotenv_vars()? {
+        std::env::set_var(key, value);
+    }
+    Ok(())
+}
+
+fn add_vars(env_content: String, vars: &mut Vec<(String, String)>) -> Result<()> {
+    let env: toml::map::Map<String, toml::Value> = toml::de::from_str(&env_content)?;
     for (key, value) in env {
         let value = match value {
             toml::Value::String(value) => value,
@@ -14,8 +31,7 @@ pub fn load_dotenv() -> anyhow::Result<()> {
             toml::Value::Float(value) => value.to_string(),
             _ => panic!("unsupported TOML value in .env.toml for key {}", key),
         };
-        std::env::set_var(key, value);
+        vars.push((key, value));
     }
-
     Ok(())
 }

.gitignore
@@ -29,3 +29,6 @@ DerivedData/
 .vscode
 .wrangler
 .flatpak-builder
+
+# Don't commit any secrets to the repo.
+.env.secret.toml

commit 4376d453c5712a83924c9e845691452ae82c8358

Fix `.env.toml` paths (#15645)
This PR fixes the `.env.toml` paths, since we inadvertently broke them
in https://github.com/zed-industries/zed/pull/15557.

There's likely a better way we can do this, but I wanted to restore the
previous behavior, for now.

Release Notes:

- N/A

crates/collab/src/bin/dotenv.rs
@@ -1,7 +1,7 @@
 use collab::env::get_dotenv_vars;
 
 fn main() -> anyhow::Result<()> {
-    for (key, value) in get_dotenv_vars()? {
+    for (key, value) in get_dotenv_vars(".")? {
         println!("export {}=\"{}\"", key, value);
     }
     Ok(())

crates/collab/src/env.rs
@@ -1,14 +1,17 @@
 use anyhow::{anyhow, Result};
 use std::fs;
+use std::path::Path;
 
-pub fn get_dotenv_vars() -> Result<Vec<(String, String)>> {
+pub fn get_dotenv_vars(current_dir: impl AsRef<Path>) -> Result<Vec<(String, String)>> {
+    let current_dir = current_dir.as_ref();
+
     let mut vars = Vec::new();
-    let env_content = fs::read_to_string("./crates/collab/.env.toml")
+    let env_content = fs::read_to_string(current_dir.join(".env.toml"))
         .map_err(|_| anyhow!("no .env.toml file found"))?;
 
     add_vars(env_content, &mut vars)?;
 
-    if let Ok(secret_content) = fs::read_to_string("./crates/collab/.env.secret.toml") {
+    if let Ok(secret_content) = fs::read_to_string(current_dir.join(".env.secret.toml")) {
         add_vars(secret_content, &mut vars)?;
     }
 
@@ -16,7 +19,7 @@ pub fn get_dotenv_vars() -> Result<Vec<(String, String)>> {
 }
 
 pub fn load_dotenv() -> Result<()> {
-    for (key, value) in get_dotenv_vars()? {
+    for (key, value) in get_dotenv_vars("./crates/collab")? {
         std::env::set_var(key, value);
     }
     Ok(())
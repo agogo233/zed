agogo233/zed

commit 7e349e52b1117931217e91a89847fd97b64164d0

Don't try and run on armv5/6/7 (#20618)
Updates: #20523

Release Notes:

- SSH Remoting: correctly show an error when SSH'ing into a 32-bit arm
system

crates/remote/src/ssh_session.rs
@@ -1553,9 +1553,13 @@ impl SshRemoteConnection {
                 "Prebuilt remote servers are not yet available for {os:?}. See https://zed.dev/docs/remote-development"
             ))?,
         };
-        let arch = if arch.starts_with("arm") || arch.starts_with("aarch64") {
+        // exclude armv5,6,7 as they are 32-bit.
+        let arch = if arch.starts_with("armv8")
+            || arch.starts_with("armv9")
+            || arch.starts_with("aarch64")
+        {
             "aarch64"
-        } else if arch.starts_with("x86") || arch.starts_with("i686") {
+        } else if arch.starts_with("x86") {
             "x86_64"
         } else {
             Err(anyhow!(

commit 61c987474ad936f8effdbc6d5872585e9426ffe6

ssh: Fix SSH to mac remotes (#20838)
Restore ability to SSH to macOS arm remotes (`uname -m` on mac == `arm64`).
Fix regression introduced in https://github.com/zed-industries/zed/pull/20618

crates/remote/src/ssh_session.rs
@@ -1568,6 +1568,7 @@ impl SshRemoteConnection {
         // exclude armv5,6,7 as they are 32-bit.
         let arch = if arch.starts_with("armv8")
             || arch.starts_with("armv9")
+            || arch.starts_with("arm64")
             || arch.starts_with("aarch64")
         {
             "aarch64"
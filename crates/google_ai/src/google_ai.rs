agogo233/zed

commit 1d0d57f31825c3ec82d1eeb7f49afc073e95d388

Introduce LanguageModelToolUse::raw_input (#29322)
This is to enable alternative streaming solutions at the application
layer. I'm not sure we really should have performed parsing of the input
at this layer. Either way I want to experiment with streaming approaches
in a separate crate on a branch, and this will help.

/cc @maxdeviant @bennetbo @rtfeldman

Closes #ISSUE

Release Notes:

- N/A

crates/google_ai/src/google_ai.rs
@@ -338,6 +338,7 @@ pub struct CountTokensResponse {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FunctionCall {
     pub name: String,
+    pub raw_args: String,
     pub args: serde_json::Value,
 }
 

crates/language_models/src/provider/google.rs
@@ -396,6 +396,7 @@ pub fn into_google(
                     Some(Part::FunctionCallPart(google_ai::FunctionCallPart {
                         function_call: google_ai::FunctionCall {
                             name: tool_use.name.to_string(),
+                            raw_args: tool_use.raw_input,
                             args: tool_use.input,
                         },
                     }))
@@ -537,6 +538,10 @@ pub fn map_to_language_model_completion_events(
                                                     id,
                                                     name,
                                                     is_input_complete: true,
+                                                    raw_input: function_call_part
+                                                        .function_call
+                                                        .raw_args
+                                                        .clone(),
                                                     input: function_call_part.function_call.args,
                                                 },
                                             )));

crates/agent/src/tool_use.rs
@@ -72,6 +72,7 @@ impl ToolUseState {
                             .map(|tool_use| LanguageModelToolUse {
                                 id: tool_use.id.clone(),
                                 name: tool_use.name.clone().into(),
+                                raw_input: tool_use.input.to_string(),
                                 input: tool_use.input.clone(),
                                 is_input_complete: true,
                             })

crates/language_model/src/language_model.rs
@@ -186,6 +186,7 @@ where
 pub struct LanguageModelToolUse {
     pub id: LanguageModelToolUseId,
     pub name: Arc<str>,
+    pub raw_input: String,
     pub input: serde_json::Value,
     pub is_input_complete: bool,
 }

crates/language_models/src/provider/bedrock.rs
@@ -894,6 +894,7 @@ pub fn map_to_language_model_completion_events(
                                                 id: tool_use.id.into(),
                                                 name: tool_use.name.into(),
                                                 is_input_complete: true,
+                                                raw_input: tool_use.input_json.clone(),
                                                 input: if tool_use.input_json.is_empty() {
                                                     Value::Null
                                                 } else {

crates/language_models/src/provider/copilot_chat.rs
@@ -368,6 +368,7 @@ pub fn map_to_language_model_completion_events(
                                                     id: tool_call.id.into(),
                                                     name: tool_call.name.as_str().into(),
                                                     is_input_complete: true,
+                                                    raw_input: tool_call.arguments.clone(),
                                                     input: serde_json::Value::from_str(
                                                         &tool_call.arguments,
                                                     )?,

crates/language_models/src/provider/open_ai.rs
@@ -491,6 +491,7 @@ pub fn map_to_language_model_completion_events(
                                                     id: tool_call.id.into(),
                                                     name: tool_call.name.as_str().into(),
                                                     is_input_complete: true,
+                                                    raw_input: tool_call.arguments.clone(),
                                                     input: serde_json::Value::from_str(
                                                         &tool_call.arguments,
                                                     )?,

commit 335606f9b101575257376f1a4ba75658a6d9bf5e

gemini: Fix issue when deserializing tool call (#29363)
Fixes a regression introduced in #29322

Release Notes:

- N/A

Co-authored-by: Agus Zubiaga <hi@aguz.me>

crates/google_ai/src/google_ai.rs
@@ -334,7 +334,6 @@ pub struct CountTokensResponse {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct FunctionCall {
     pub name: String,
-    pub raw_args: String,
     pub args: serde_json::Value,
 }
 

crates/language_models/src/provider/google.rs
@@ -396,7 +396,6 @@ pub fn into_google(
                     Some(Part::FunctionCallPart(google_ai::FunctionCallPart {
                         function_call: google_ai::FunctionCall {
                             name: tool_use.name.to_string(),
-                            raw_args: tool_use.raw_input,
                             args: tool_use.input,
                         },
                     }))
@@ -540,8 +539,8 @@ pub fn map_to_language_model_completion_events(
                                                     is_input_complete: true,
                                                     raw_input: function_call_part
                                                         .function_call
-                                                        .raw_args
-                                                        .clone(),
+                                                        .args
+                                                        .to_string(),
                                                     input: function_call_part.function_call.args,
                                                 },
                                             )));
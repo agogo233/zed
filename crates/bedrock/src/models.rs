agogo233/zed

commit d86789774634a9651aabdfd1a75f0972b252252b

bedrock: Support cross-region inference for US Claude 3.5 Haiku (#28523)
Release Notes:

- Added Cross-Region inference support for US Claude 3.5 Haiku

Co-authored-by: Peter Tripp <peter@zed.dev>
Co-authored-by: Marshall Bowers <git@maxdeviant.com>

crates/bedrock/src/models.rs
@@ -321,6 +321,7 @@ impl Model {
 
             // Models available only in US
             (Model::Claude3Opus, "us")
+            | (Model::Claude3_5Sonnet, "us")
             | (Model::Claude3_7Sonnet, "us")
             | (Model::Claude3_7SonnetThinking, "us")
             | (Model::AmazonNovaPremier, "us") => Ok(format!("{}.{}", region_group, model_id)),

commit 3a95ae9e5439e4bb88260ea06cfbf0bbbc62156e

bedrock: Fix Claude 3.5 Haiku support (#30560)
This PR corrects a mistake introduced in
https://github.com/zed-industries/zed/pull/28523.

https://github.com/zed-industries/zed/pull/28523#issuecomment-2872369707

Release Notes:

- N/A

crates/bedrock/src/models.rs
@@ -324,7 +324,7 @@ impl Model {
 
             // Models available only in US
             (Model::Claude3Opus, "us")
-            | (Model::Claude3_5Sonnet, "us")
+            | (Model::Claude3_5Haiku, "us")
             | (Model::Claude3_7Sonnet, "us")
             | (Model::Claude3_7SonnetThinking, "us")
             | (Model::AmazonNovaPremier, "us")
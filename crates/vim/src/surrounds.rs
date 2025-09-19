agogo233/zed

commit c89435154450e8d8878f6f03ff39c772b1cfc503

vim: Fix change surrounding quotes with whitespace within (#37321)
This commit fixes a bug with Zed's vim mode surrounds plugin when
dealing with replacing pairs with quote and the contents between the
pairs had some whitespace within them.

For example, with the following string:

```
' str '
```

If one was to use the `cs'"` command, to replace single quotes with
double quotes, the result would actually be:

```
"str"
```

As the whitespace before and after the closing character was removed.

This happens because of the way the plugin decides whether to add or
remove whitespace after and before the opening and closing characters,
repsectively. For example, using `cs{[` yields a different result from
using `cs{]`, the former adds a space while the latter does not.

However, since for quotes the opening and closing character is exactly
the same, this behavior is not possible, so this commit updates the code
in `vim::surrounds::Vim.change_surrounds` so that it never adds or
removes whitespace when dealing with any type of quotes.

Closes #12247 

Release Notes:

- Fixed whitespace handling when changing surrounding pairs to quotes in
vim mode

crates/vim/src/surrounds.rs
@@ -240,7 +240,24 @@ impl Vim {
                             newline: false,
                         },
                     };
-                    let surround = pair.end != surround_alias((*text).as_ref());
+
+                    // Determines whether space should be added/removed after
+                    // and before the surround pairs.
+                    // For example, using `cs{[` will add a space before and
+                    // after the pair, while using `cs{]` will not, notice the
+                    // use of the closing bracket instead of the opening bracket
+                    // on the target object.
+                    // In the case of quotes, the opening and closing is the
+                    // same, so no space will ever be added or removed.
+                    let surround = match target {
+                        Object::Quotes
+                        | Object::BackQuotes
+                        | Object::AnyQuotes
+                        | Object::MiniQuotes
+                        | Object::DoubleQuotes => true,
+                        _ => pair.end != surround_alias((*text).as_ref()),
+                    };
+
                     let (display_map, selections) = editor.selections.all_adjusted_display(cx);
                     let mut edits = Vec::new();
                     let mut anchors = Vec::new();
@@ -1126,6 +1143,30 @@ mod test {
                     println!(\"it is fine\");
                 ]
             ];"},
+            Mode::Normal,
+        );
+
+        // test change quotes.
+        cx.set_state(indoc! {"'  ˇstr  '"}, Mode::Normal);
+        cx.simulate_keystrokes("c s ' \"");
+        cx.assert_state(indoc! {"ˇ\"  str  \""}, Mode::Normal);
+
+        // test multi cursor change quotes
+        cx.set_state(
+            indoc! {"
+            '  ˇstr  '
+            some example text here
+            ˇ'  str  '
+        "},
+            Mode::Normal,
+        );
+        cx.simulate_keystrokes("c s ' \"");
+        cx.assert_state(
+            indoc! {"
+            ˇ\"  str  \"
+            some example text here
+            ˇ\"  str  \"
+        "},
             Mode::Normal,
         );
     }

commit 2d137dea67a738d86c34bca9badd80f05a7c322e

vim: Fix regression in surround behavior (#38344)
Fix an issue introduced in
https://github.com/zed-industries/zed/pull/37321 where vim's surround
wouldn't work as expected when replacing quotes with non-quotes, with
whitespace always being added, regardless of whether the opening or
closing bracket was used. This is not the intended, or previous,
behavior, where only the opening bracket would trigger whitespace to be
added.

Closes #38169 

Release Notes:

- Fixed regression in vim's surround plugin that ignored whether the
opening or closing bracket was being used when replacing quotes, so
space would always be added

crates/vim/src/surrounds.rs
@@ -241,21 +241,15 @@ impl Vim {
                         },
                     };
 
-                    // Determines whether space should be added/removed after
+                    // Determines whether space should be added after
                     // and before the surround pairs.
-                    // For example, using `cs{[` will add a space before and
-                    // after the pair, while using `cs{]` will not, notice the
-                    // use of the closing bracket instead of the opening bracket
-                    // on the target object.
-                    // In the case of quotes, the opening and closing is the
-                    // same, so no space will ever be added or removed.
-                    let surround = match target {
-                        Object::Quotes
-                        | Object::BackQuotes
-                        | Object::AnyQuotes
-                        | Object::MiniQuotes
-                        | Object::DoubleQuotes => true,
-                        _ => pair.end != surround_alias((*text).as_ref()),
+                    // Space is only added in the following cases:
+                    // - new surround is not quote and is opening bracket (({[<)
+                    // - new surround is quote and original was also quote
+                    let surround = if pair.start != pair.end {
+                        pair.end != surround_alias((*text).as_ref())
+                    } else {
+                        will_replace_pair.start == will_replace_pair.end
                     };
 
                     let (display_map, selections) = editor.selections.all_adjusted_display(cx);
@@ -1241,6 +1235,15 @@ mod test {
         "},
             Mode::Normal,
         );
+
+        // test quote to bracket spacing.
+        cx.set_state(indoc! {"'ˇfoobar'"}, Mode::Normal);
+        cx.simulate_keystrokes("c s ' {");
+        cx.assert_state(indoc! {"ˇ{ foobar }"}, Mode::Normal);
+
+        cx.set_state(indoc! {"'ˇfoobar'"}, Mode::Normal);
+        cx.simulate_keystrokes("c s ' }");
+        cx.assert_state(indoc! {"ˇ{foobar}"}, Mode::Normal);
     }
 
     #[gpui::test]